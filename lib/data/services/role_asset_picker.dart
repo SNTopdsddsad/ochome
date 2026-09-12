461d0778eceaf35092025e1f7bdcb682feec4ef3
丹青 <xuwudi404@gmail.com>
2026-09-12 15:35:58 +0800

    应用: 修复安卓相册添加打开文件管理器

--- lib/data/services/role_asset_picker.dart
+++ lib/data/services/role_asset_picker.dart
@@ -1,10 +1,17 @@
 import 'package:file_selector/file_selector.dart' as selector;
 import 'package:image_picker/image_picker.dart';
+import 'package:image_picker_android/image_picker_android.dart';
+import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
 
 /// 系统选择器的薄封装，页面测试可以注入取消、成功或异常结果。
 class RoleAssetPicker {
-  Future<List<XFile>> pickMedia() =>
-      ImagePicker().pickMultipleMedia(requestFullMetadata: false);
+  Future<List<XFile>> pickMedia() {
+    final platform = ImagePickerPlatform.instance;
+    if (platform is ImagePickerAndroid) {
+      platform.useAndroidPhotoPicker = true;
+    }
+    return ImagePicker().pickMultipleMedia(requestFullMetadata: false);
+  }
 
   Future<List<XFile>> pickFiles() =>
       selector.openFiles(confirmButtonText: '添加资产');
